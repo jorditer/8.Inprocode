9e4e6a0843c553234cff8eec68b10b86eab45d25
jorditer <jordi.terradas6@gmail.com>
2024-11-29 18:13:10 +0100

    add front

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import '../index.css'
+
+function App() {
+
+  return (
+   <div className="flex justify-center p-5">
+    <h1 className="mx-auto bg-slate-300 font-bold p-6 rounded-2xl">Testing</h1>
+   </div> 
+  )
+}
+
+export default App
