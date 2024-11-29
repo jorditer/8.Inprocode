d810a4c7c8d90a9afdf8137fab74411576ac88f8
jorditer <jordi.terradas6@gmail.com>
2024-11-29 16:36:54 +0100

    back

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ mongoose.connect(mongoUrl)
     .then(() => {
         console.log('Connected to MongoDB!');
         app.listen(PORT, () => {
-            console.log('Server started on http://localhost:3000');
+            console.log(`Server started on http://localhost:${PORT}`);
         });
     })
     .catch((error) => {
